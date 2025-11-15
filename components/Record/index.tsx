a78e5e09d4cf3ef6d6400e63709c962249da57b8
Acuiza <Acuiza@hypernovalabs.com>
2025-11-15 17:33:06 -0400

    create component Record

---
+++ components/Record/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+interface RecordProps {
+    title: string;
+    textRecord: string;
+    timeRecord: number;
+}
+const Record = ({title, textRecord, timeRecord}: RecordProps) => {
+  return (
+    <View
+      style={{
+        width: '90%',
+        alignItems: 'flex-start',
+        backgroundColor: 'white',
+        borderRadius: 10,
+        marginTop: 20,
+        padding: 10,
+        shadowColor: 'gray',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.8,
+        shadowRadius: 5,
+      }}>
+      <Text
+        numberOfLines={1}
+        style={{
+          fontSize: 20,
+          fontWeight: 'bold',
+          margin: 10,
+          width: '70%',
+        }}>
+       {title}
+      </Text>
+      <View style={{paddingHorizontal: 10, display: 'flex'}}>
+        <Text style={{color: 'gray'}} numberOfLines={4}>{textRecord}</Text>
+      </View>
+      <Text style={{paddingHorizontal: 10}}>
+        {timeRecord} minutes
+      </Text>
+    </View>
+  );
+};
+export default Record;
