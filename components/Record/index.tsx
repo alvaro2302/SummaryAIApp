2eaf0afb8bec2ed6b74602adb1bd43d5734a8b79
Acuiza <Acuiza@hypernovalabs.com>
2025-11-15 18:26:10 -0400

    create component Record

--- components/Record/index.tsx
+++ components/Record/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-interface RecordProps {
+export interface RecordProps {
     title: string;
     textRecord: string;
     timeRecord: number;
